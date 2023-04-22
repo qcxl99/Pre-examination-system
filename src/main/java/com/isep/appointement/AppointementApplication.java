87a98eb64b832374b0e5673c98d8c4038ec2068c
zhang <zhangkex1218@gmail.com>
2023-04-22 23:36:44 +0200

    Web projet first time commit

---
+++ src/main/java/com/isep/appointement/AppointementApplication.java
@@ -0,0 +1,13 @@
+package com.isep.appointement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isep/appointement/AppointementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isep.appointement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
