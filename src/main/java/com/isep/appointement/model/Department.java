bff8029e48a13ec692a61224148231bd2286beca
qcxl99 <1550060541@qq.com>
2023-05-28 18:50:13 +0200

    index.html,http://localhost:8080/, href = "/css/..."

---
+++ src/main/java/com/isep/appointement/model/Department.java
@@ -0,0 +1,51 @@
+package com.isep.appointement.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "doctor")
+    private Doctor doctor;
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+}
